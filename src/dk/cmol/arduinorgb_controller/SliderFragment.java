15e7cde1873748ff28610727aa106e2bda8cb1d6
Claus Lensbøl <cmol@cmol.dk>
2014-01-13 21:50:12 +0100

    Added a lot of seek bar stuff to the slider fragment

--- src/dk/cmol/arduinorgb_controller/SliderFragment.java
+++ src/dk/cmol/arduinorgb_controller/SliderFragment.java
@@ -1,20 +1,66 @@
 package dk.cmol.arduinorgb_controller;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SliderFragment extends Fragment {
+public class SliderFragment extends Fragment implements OnSeekBarChangeListener {
+
+	ToolbarListener activityCallback;
+	
+	public interface ToolbarListener {
+		public void seekChange(String colors);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			activityCallback = (ToolbarListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement ToolbarListener");
+		}
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_slider, container,
 				false);
+		
+		SeekBar seekBarR = (SeekBar) rootView.findViewById(R.id.seekBarR);
+		SeekBar seekBarG = (SeekBar) rootView.findViewById(R.id.seekBarG);
+		SeekBar seekBarB = (SeekBar) rootView.findViewById(R.id.seekBarB);
+		
+		seekBarR.setOnSeekBarChangeListener(this);
+		seekBarG.setOnSeekBarChangeListener(this);
+		seekBarB.setOnSeekBarChangeListener(this);
 
 		return rootView;
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		String cols = Integer.toString(((SeekBar)getActivity().findViewById(R.id.seekBarR)).getProgress()*5)+"-";
+		cols += Integer.toString(((SeekBar)getActivity().findViewById(R.id.seekBarG)).getProgress()*5)+"-";
+		cols += Integer.toString(((SeekBar)getActivity().findViewById(R.id.seekBarR)).getProgress()*5);
+		activityCallback.seekChange(cols);
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
 	}
 
 }
