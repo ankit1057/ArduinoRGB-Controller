6e2cd119dc6054d58118bbeced1809e202745510
Claus Lensbøl <cmol@cmol.dk>
2014-01-14 13:43:15 +0100

    Finally fix the threading model using handelers. Needs cleanup

--- src/dk/cmol/arduinorgb_controller/ArduinoSocket.java
+++ src/dk/cmol/arduinorgb_controller/ArduinoSocket.java
@@ -5,16 +5,20 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class ArduinoSocket {
+public class ArduinoSocket extends Thread{
 	
 	private ArduinoRGBActivity parent = null;
 	private Socket sock = null;
 	private OutputStream stream = null;
 	private String ip = null;
 	private int port;
+	public Handler mHandler;
 	
 	public ArduinoSocket(ArduinoRGBActivity parent) {
 		this.parent = parent;
@@ -38,6 +42,31 @@ public class ArduinoSocket {
 		});
 
 		thread.start();
+	}
+	
+	public void run() {
+		connect();
+		
+		Looper.prepare();
+
+        mHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    try {
+						stream.write((byte[]) msg.obj);
+						stream.write((byte) 10);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                }
+        };
+        Looper.loop();
+	}
+	
+	public void writeMessage(final byte[] message) {
+		Message msg = Message.obtain();
+		msg.obj = message;
+		mHandler.sendMessage(msg);
 	}
 	
 	private void connect() {
