b9474130469dc0cdd44afbdb36f839d17cef9d6f
Claus Lensbøl <cmol@cmol.dk>
2014-01-14 15:41:48 +0100

    Add value information to the numberPickers

--- src/dk/cmol/arduinorgb_controller/FaderFragment.java
+++ src/dk/cmol/arduinorgb_controller/FaderFragment.java
@@ -5,14 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 public class FaderFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_fader,
-				container, false);
+		View rootView = inflater.inflate(R.layout.fragment_fader, container,
+				false);
+
+		// Add min and max to number picker elements
+		NumberPicker[] nps = {(NumberPicker) rootView.findViewById(R.id.numberPicker1),
+				(NumberPicker) rootView.findViewById(R.id.numberPicker2),
+				(NumberPicker) rootView.findViewById(R.id.numberPicker3)
+		};
+		
+		for (int i = 0; i < nps.length; i++) {
+			nps[i].setMaxValue(255);
+			nps[i].setMinValue(0);
+		}
+		
 
 		return rootView;
 	}
