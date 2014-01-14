edd892071efd8d69658a22ef2aa4fa2dc21ef01e
Claus Lensbøl <cmol@cmol.dk>
2014-01-14 14:33:52 +0100

    Remove unused imports

--- src/dk/cmol/arduinorgb_controller/ColorDotsFragment.java
+++ src/dk/cmol/arduinorgb_controller/ColorDotsFragment.java
@@ -1,13 +1,10 @@
 package dk.cmol.arduinorgb_controller;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class ColorDotsFragment extends Fragment {
 
@@ -16,20 +13,6 @@ public class ColorDotsFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_color_dots,
 				container, false);
-
-		boolean[] lamps = ((ArduinoRGBActivity) getActivity()).lamp_toggle;
-
-		Button btns[] = { (Button) getActivity().findViewById(R.id.lamp1),
-				(Button) getActivity().findViewById(R.id.lamp2),
-				(Button) getActivity().findViewById(R.id.lamp3),
-				(Button) getActivity().findViewById(R.id.lamp4) };
-
-		for (int i = 0; i < lamps.length; i++) {
-			if (lamps[i]) {
-				btns[i].getBackground().setColorFilter(Color.GREEN,
-						PorterDuff.Mode.MULTIPLY);
-			}
-		}
 
 		return rootView;
 	}
