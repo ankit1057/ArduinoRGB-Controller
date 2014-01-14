239aabd2c60a79ad1aedb1c068113aa1977010d8
Claus Lensbøl <cmol@cmol.dk>
2014-01-14 14:55:01 +0100

    Add fader view

---
+++ src/dk/cmol/arduinorgb_controller/FaderFragment.java
@@ -0,0 +1,19 @@
+package dk.cmol.arduinorgb_controller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FaderFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_fader,
+				container, false);
+
+		return rootView;
+	}
+}
